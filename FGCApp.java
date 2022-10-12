059661a14a283195b69a7ed3cbcb5d6e768251b0
joeyCampbelll <joey.m.campbell@gmail.com>
2022-10-12 14:25:29 -0700

    joey-feature-baseline-ui initial commit

--- FGCApp.java
+++ FGCApp.java
@@ -2,9 +2,7 @@
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
-import javafx.scene.layout.Border;
 import javafx.scene.layout.BorderPane;
-import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 // java lib imports
